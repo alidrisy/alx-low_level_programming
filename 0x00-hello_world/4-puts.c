e7f1f466d9eeb10abb37d47ee46aaaed96f8492f
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-17 01:28:19 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 
-int main(void)
 /**
-*main=startpoint
-*description="print a string"
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
 */
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
