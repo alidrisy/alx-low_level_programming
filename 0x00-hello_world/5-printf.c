daf7b20af4ecdcc62008b984b7802151ea8e5313
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-16 15:10:52 -0700

    add

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#includ<stdio.h>
+
+int main(void)
+/**
+* main - print
+* @main:startpoint
+* "print a string"
+*/
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
