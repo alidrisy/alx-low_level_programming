decfb740e6f9ca9904b5e9d70248d5eba22c43cb
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-16 15:50:51 -0700

    add

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+char a;
+int b;
+fluot c;
+printf("Size of a char: %lu %n",(unsigned long)sizeof(a));
+printf("Size of an int: %lu %n",(unsigned long)sizeof(b));
+printf("Size of a long int: %lu %n",(long)sizeof(b));
+printf("Size of a long long int: %lu %n",(long long)sizeof(b));
+printf("Size of a flout: %lu %n",(unsigned long)sizeof(c));
+return (0);
+}
