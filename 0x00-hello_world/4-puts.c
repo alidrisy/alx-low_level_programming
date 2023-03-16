1df30392f23989dfd1c5b95c36325ce578c67ce4
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-16 14:34:00 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 
-#include<stdio.h>
+include<stdio.h>
 int main(void)
-/*
-* main 
-* A C program that print "Programming is like building a multilingual puzzle
-*return 0
+/**
+* main-starpoint
+*description  main-starpoint
+*description=="a C program that print "Programming is like building a multilingual puzzle"
 */
 {
 puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
+return(0);
 }
