3103ab58f517a38ef3dc4006ae4230dc929c59a2
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-17 01:55:42 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,28 @@
-#include<stdio.h>
+#include <stdio.h> 
+
+  
+
+ /** 
+
+  * main - Prints the size of various types based on 
+
+  *        the computer it is compiled and run on.. 
+
+  * 
+
+  * Return: Always 0. 
+
+  */
 
 int main(void)
 {
 char a;
 int b;
 fluot c;
-printf("Size of a char: %lu %n",(unsigned long)sizeof(a));
-printf("Size of an int: %lu %n",(unsigned long)sizeof(b));
-printf("Size of a long int: %lu %n",(long)sizeof(b));
-printf("Size of a long long int: %lu %n",(long long)sizeof(b));
-printf("Size of a flout: %lu %n",(unsigned long)sizeof(c));
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", (long)sizeof(b));
+printf("Size of a long long int: %zu byte(s)\n", (long long)sizeof(b));
+printf("Size of a flout: %zu byte(s)\n", (unsigned long)sizeof(c));
 return (0);
 }
