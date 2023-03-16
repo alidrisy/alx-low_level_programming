55681777cd8c55a3ec8a40b5d10cf7fc48ee62f5
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-16 13:40:17 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,2 @@
-#include <stdio.h>
-
-int main()
-{
-  puts("Programming is like building a multilingual puzzle\n");
-  return 0;
-}
+#include<stdio.h>
+int main(void) /*C program that prints*/ { puts("\"Programming is like building a multilingual puzzle\n"); return 0; }
