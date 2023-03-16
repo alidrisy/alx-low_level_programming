180e43d3ccc20b85d56ce0d2e06818da08647f2e
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-17 01:17:50 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@ int main(void)
 /**
 * main - print
 * @main:startpoint
-* "print a string"
+* 'print a string'
 */
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
