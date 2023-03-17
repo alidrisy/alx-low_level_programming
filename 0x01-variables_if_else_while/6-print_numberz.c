c31aae8bd18cf425c2eed67c2663c2030b308ca9
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-17 14:38:39 -0700

    add

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+* main - prints the number in lowercase
+* use just putchar
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+printf("%d", number);
+
+printf("\n");
+
+return (0);
+}
