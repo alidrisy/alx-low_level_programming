71d2db9e84197836c6a220d152917f2d363cf2c2
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-18 01:26:03 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,16 @@ int main(void)
 {
 int number;
 for (number = 0; number < 10; number++)
+{
 putchar((number % 10) + '0');
-
+if (number < 9)
+{
+putchar(',');
+putchar(' ');
+}
 putchar('\n');
-
+}
 return (0);
 }
+
+  
