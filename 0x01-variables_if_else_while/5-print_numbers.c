c2032055b10ab57f7238d0e3c3c51bb5ee3d7565
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-17 14:26:13 -0700

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+* main - prints the alphabet in lowercase
+* use just putchar
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
