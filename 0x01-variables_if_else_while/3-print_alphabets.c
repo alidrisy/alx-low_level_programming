9575f5234a80b1287c7454d07b1e03095d7cb80a
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-18 00:12:45 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,10 @@
 int main(void)
 {
 char letter;
-for (letter = 'a..A'; letter <= 'z..Z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
 putchar('\n');
 
 return (0);
