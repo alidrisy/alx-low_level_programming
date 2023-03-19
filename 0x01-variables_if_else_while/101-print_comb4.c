59df8e0974b560821cad72d8a4f609a4da2890ff
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-19 07:11:09 -0700

    add

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+* main - prints the number in lowercase
+* use just putchar
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int number, number2;
+for (number = 0; number < 10; number++)
+{
+for (number2 = number + 1; number2 < 10; number2++)
+{
+putchar((number % 10) + '0');
+putchar((number2 % 10) + '0');
+if (number == 8 && number2 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
+
