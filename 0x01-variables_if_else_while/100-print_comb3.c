15817818cabaa9e6ad3b41267b0f7b7f3a54d742
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-19 07:00:47 -0700

    add

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+* main - prints the number in lowercase
+* use just putchar
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+{
+putchar((number % 10) + '0');
+if (number < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
+
