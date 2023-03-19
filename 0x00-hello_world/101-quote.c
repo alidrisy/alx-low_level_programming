189847fd21d28cd0def260c4320aded12cb779c0
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-19 20:59:33 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+* main - writ string
+* just write contant
+*
+* RETURN: ALWAYS 0;
+*/
+int main(void)
+{
+putchar('and that piece of art is useful" - Dora Korpar, 2015-10-19');
+return (0);
+}
