c8b838e074abc4acfdc5dd87518a5901563edb59
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-19 17:14:50 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,17 +8,22 @@
 */
 int main(void)
 {
-int number, number2;
+int number, number2, number3;
 for (number = 0; number < 10; number++)
 {
 for (number2 = number + 1; number2 < 10; number2++)
 {
+for (number3 = number2 + 1; number3 < 10; number3++)
+{
 putchar((number % 10) + '0');
 putchar((number2 % 10) + '0');
+putchar((number3 % 10) + '0');
+
 if (number == 8 && number2 == 9)
 continue;
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
