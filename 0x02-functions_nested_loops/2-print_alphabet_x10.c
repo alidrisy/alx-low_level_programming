2dda98049d2f4a93c7dee9bac6999e48913bca32
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-21 14:33:10 -0700

    main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+/**
+* print_alphabet - prints the alphabet in lowercase, followed by a new line.
+*/
+void print_alphabet(void)
+
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+_putchar(x);
+_putchar('\n');
+}
+
