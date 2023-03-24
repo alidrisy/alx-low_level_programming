a47213e227b174e125994dd7e84f34465669c52a
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-25 02:08:31 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,12 +6,12 @@
 * @y: The second number to multiply with first number
 *
 * Return: The result of multiplication between x param and y param
+*/
 
 int mul(int a, int b)
 {
 return (a * b);
 }
-
 
 
 
