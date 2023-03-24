e3a6bfa96883f6239167b2df416ea942deb54cd6
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-24 15:53:05 -0700

    add

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - Check if a letter is upper
+* @c: The number to be checked
+*
+* Return: 1 for digit or 0 for any else
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+
+return (0);
+
+}
