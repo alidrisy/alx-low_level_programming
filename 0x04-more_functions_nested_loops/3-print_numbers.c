e0690e7570b16df95d528740bed50c9a125805b1
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-24 16:25:39 -0700

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the numbers, from 0 to 9
+* add a new line 
+*
+* Return: 0
+void print_numbers(void)
+{
+for (x = 0; x >= 10; x++)
+putchar((x % 10) + 0);
+putchar(n);
+return (0);
+}
