77a1836b401bb84425c06371965ece622237e1ca
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-25 01:29:43 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
 * main - hecks for uppercase character
+* @c: The number to be checked
+*
 * Return: 1 for upper letter or 0 for any else
 */
 int _isupper(int c)
 {
+
 if (c >= 65 && c <= 90)
 {
 return (1);
 }
-
 return (0);
 }
 
