609800dca17fc08de370df3af54e9aa4c850e967
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-26 02:06:16 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,28 @@ for (x = 1; x <= 100; x++)
 {
 
 if ((x % 3) == 0)
-
+{
 printf("Fizz");
-
-if ((x % 5) == 0)
-
+}
+else if (x % 5 == 0)
+{
 printf("Buzz");
-
-if ((x % 3) == 0 && (x % 5) == 0)
-
+}
+else if ((x % 3 == 0) && (x % 5 == 0))
+{
 printf("FizzBuzz");
-
+}
 else
+{
 printf("%d", x);
-if (x == 100)
-continue;
+}
+if (x != 100)
+{
 printf(" ");
 }
+}
 printf("\n");
+
+
 return (0);
 }
