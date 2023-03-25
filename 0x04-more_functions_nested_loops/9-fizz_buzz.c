4ad7c0922883ee4ec44421da18e7b7d2634270f8
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-26 01:37:15 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,29 @@
- 
+#include<stdio.h>
+
+/**
+* main - print fizz and buzz and fizzbuzz
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int x;
+for (x = 1; x <= 100; x++)
+{
+printf("%d ", x);
+if (x % 3)
+{
+printf("Fizz ");
+}
+if (x % 5)
+{
+printf("Buzz ");
+}
+ if ((x % 3) && (x % 5))
+  {
+  printf("FizzBuzz ");
+  }
+ }
+ return (0);
+ }
