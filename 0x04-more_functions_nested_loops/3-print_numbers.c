da067cb1ae332e317af74786b26f8f22f7e1a901
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-25 03:18:14 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,14 @@
 * 
 * Return: The numbers since 0 up to 9
 */
-void print_numbers(void)
+void print_numbers(void);
 {
-int x;
+int number;
 
-for (x = 0; x <= 9; x++)
+for (number = 0; number <= 9; number++)
 {
-_putchar(x + 0);
+_putchar((number % 10) + '0');
 }
 
-_putchar(n);
+_putchar('\n');
 }
