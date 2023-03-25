4e608003b1cd9d0c1221220a0bfd52d6a5d61a28
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-25 03:49:55 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 
 int number;
 
-for (number = 48; number <= 57; number++)
+for (number = 48; number < 58; number++)
 {
 if (number != 50 && number != 52)
 _putchar(number + '0');
