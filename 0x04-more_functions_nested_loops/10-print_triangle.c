978ec978338819df09d91f1a39a3a4083e2ce61e
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-25 16:49:16 -0700

    add

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_square - print line.
+*@size: the number of \ character.
+*/
+void print_square(int size)
+{
+int x, z;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+if (x >= 0)
+{
+for (z = 0; z < size; z++)
+_putchar('#');
+}
+
+_putchar('\n');
+}
+
+}
+}
