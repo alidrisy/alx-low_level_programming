aa46a5d0dd5ee3a2055b540e867add6f5a69e5de
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-25 23:28:52 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,17 @@
- 
+#include "main.h"
+
+/**
+* print_diagonal - print line.
+*/
+void print_diagonal(int n)
+{
+int x;
+if (n > 0)
+{
+for (x = 0; x < n; x++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
