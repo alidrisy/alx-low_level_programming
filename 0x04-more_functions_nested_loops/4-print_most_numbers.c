ac759a1c506f6f85f49e9c58117d86840921e62f
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-24 17:23:14 -0700

    add

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - Print the numbers since 0 to 9 
+* 
+* Return: The numbers since 0 up to 9
+*/
+void print_numbers(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+{
+_putchar((number % 10) + '0');
+}
+
+_putchar('\n');
+}
