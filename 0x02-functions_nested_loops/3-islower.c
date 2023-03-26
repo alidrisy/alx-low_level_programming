4f3c605c509ea06f8fc9c0ab9134d40103450ed0
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-27 01:35:29 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,18 @@
- 
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character.
+* @c: a variable
+*/
+
+int _islower(int c)
+{
+if (_islower(c))
+{
+_putchar('1');
+}
+else
+{
+_putchar('0');
+}
+}
