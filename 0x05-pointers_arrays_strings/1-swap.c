c73490a543f51233100412a3926e190f5b127b14
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-28 02:37:24 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
 * swap_int - swaps the values of two integers.
-* @a: is the verst integer
-* @b: is rhe second integer
+* @a: is the verst integer.
+* @b: is rhe second integer.
 */
 
 void swap_int(int *a, int *b)
