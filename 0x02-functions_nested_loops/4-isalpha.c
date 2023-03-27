550a63685d539d100358b41df1c96d08986a555e
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-26 17:18:39 -0700

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character.
+* @c: a variable
+* Return: if is lower 1 else 0.
+*/
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+
+return (0);
+
+}
