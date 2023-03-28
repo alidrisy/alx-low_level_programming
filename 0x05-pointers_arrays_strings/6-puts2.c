8999009353dab55cdd3c4bf82ca2595341849c37
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-28 14:06:49 -0700

    add

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* puts2 - prints every other character of a string
+* @str: a variable string
+*/
+void puts2(char *str)
+{
+int x = 0, c = 0;
+while (str[x] != 0)
+{
+x++;
+}
+for(c = 0; c < x; c += 2)
+_putchar(str[c]);
+}
