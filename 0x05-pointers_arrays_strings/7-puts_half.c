186d2dfb96ce69bc163b2331a9376e72a10aeaab
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-29 00:37:46 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,19 @@ while (str[x] != 0)
 {
 x++;
 }
-for (c = (x / 2); c < x; c++)
+if (x % 2 == 1)
+{
+c = (x - 1) / 2;
+c += 1;
+}
+else
+{
+c = x / 2;
+}
+
+
+
+for (; c < x; c++)
 _putchar(str[c]);
 _putchar(n);
 }
