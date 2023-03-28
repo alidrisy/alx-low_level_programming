10acd753838d6d0c7d7964a060f803a54b511316
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-03-28 15:51:09 -0700

    add

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* *_strcpy - copies the string pointed to by src
+* @dest: a pointer 
+* @src: second pointer
+* Return: a value of variable @dest
+*)
+void print_array(int *a, int n)
+{
+int x;
+while(src[x])
+{
+dest[x] = src[x];
+x++;
+}
+return (src);
+}
