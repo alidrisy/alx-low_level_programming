a5aa736e2757e2f5771931c80b1b268494340d0c
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-03 14:25:16 -0700

    add

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* _memset - fills memory with a constant byte.
+* @s: a pointer
+* @b: a second pointer
+* @n: integer variable
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
