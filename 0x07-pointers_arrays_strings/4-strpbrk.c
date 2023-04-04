58db9cded057b88672b7523dcdf7e47a938508a7
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-04 14:56:35 -0700

    add

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* _strspn - gets the length of a prefix substring.
+* @s: char pointer
+* @accept: char pointer
+* Return: char with result
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int x = 0, v = 0;
+unsigned int c = 0;
+while (s[x] != ' ' && s[x] != '\0')
+{
+
+while (accept[v] != '\0')
+{
+
+if (s[x] == accept[v])
+c++;
+v++;
+}
+x++;
+v = 0;
+}
+return (c);
+}
+
+
+
+
+
