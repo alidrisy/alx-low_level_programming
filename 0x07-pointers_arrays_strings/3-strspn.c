ca4fa9170ed78e5e5e6a7a3938e7e987354c62f0
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-04 12:31:30 -0700

    add

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* _strspn - gets the length of a prefix substring.
+* @s: char pointer
+* @accept: char pointer
+* Return: s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
