a95f8751e30aacaabbf22ccc2156a3bdcfab10bd
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-06 05:14:22 +0300

    Is prime number with recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,38 +1,38 @@
 #include "main.h"
-int _root(int n, int x);
+int _prime(int n, int x);
 /**
-* _sqrt_recursion - get the real square root of number
+* is_prime_number - get the real square root of number
 * @n: a number that have to get root
-* Return: 0 if n is not real
+* Return: 0 if n is not prime number
 */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
 if (n < 0)
 {
-return (-1);
+return (0);
 }
 else if (n == 0 || n == 1)
 {
-return (n);
+return (0);
 }
 else
-return (_root(n, 1));
+return (_prime(n, 1));
 }
 /**
-* _root - return the n's root
+* _prime - return the 1 if it's prime number
 * @n: the main number
 * @x: an integer
-* Return: a value of x
+* Return: 1 or 0
 */
-int _root(int n, int x)
+int _prime(int n, int x)
 {
 if (x * x == n)
 {
-return (x);
+return (0);
 }
-else if (x * x > n)
+else if (x * x > n || x * x < n)
 {
-return (-1);
+return (1);
 }
 return (_root(n, x + 1));
 }
