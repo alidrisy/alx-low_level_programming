2586eb15c902a76def7986b248cbd8d51bf8cf6c
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-05 18:20:29 -0700

    add

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _sqrt_recursion - get the real square root of number
+* @n: a number that have to get root
+* Return: n root
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
