e441305db37bf60a4c84d60cd11ea4fc9955de3f
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-05 17:23:35 -0700

    power with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _pow_recursion - returns the value of x raised to the power of y.
+* @x: the int variable number
+* @y: the int power number
+* Return: x power y
+*/
+int _pow_recursion(int x, int y)
+{
+int v;
+v = pow(x, y);
+return (v);
+}
