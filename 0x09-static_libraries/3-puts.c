e65667528e24cfbccc72e550c83a704d1f8b7a08
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-09 23:42:39 +0300

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+* _puts - print a string
+* @s: a character variable
+*/
 void _puts(char *s)
 {
 int i;
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; *s != '\0'; i++)
 {
-_putchar(s[i]);
+_putchar(*s);
+s++;
 }
 _putchar('\n');
 }
