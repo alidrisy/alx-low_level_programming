3bb508d286894e6199f8f71f8b91c612b200b41b
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-10 02:19:14 +0300

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* *_memcpy - function copies n bytes from memory 
+* @dest: cahr
+* @src: char
+* @n: int
+* Return:0
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 return (0);
