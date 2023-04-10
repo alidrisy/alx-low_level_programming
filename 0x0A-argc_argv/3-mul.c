52457710e80516c813686b035a6d59cf954cf264
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-11 00:21:33 +0300

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,18 @@
 int main(int argc, char *argv[])
 {
 int i, x = 1;
-if (argc > 1 && argc <= 2)
+if (argc == 1 && argc == 2)
+{
+printf("Error\n");
+return (1);
+}
+else
 {
 for (i = 1; i < 3; i++)
 {
 x *= atoi(argv[i]);
 }
 printf("%d\n", x);
-}
-else if (argc == 1 || argc 
-{
-printf("Error\n");
-return (1);
 }
 return (0);
 }
