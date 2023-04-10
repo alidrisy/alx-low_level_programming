a80ce9a86e5698b5f2e176d6456d4dce25ab5c78
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-11 00:02:11 +0300

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,9 @@
 int main(int argc, char *argv[])
 {
 int i, x = 1;
-if (argc > 1 && argc < 2)
+if (argc > 1 && argc < 3)
 {
-for (i = 1; i <= argc; i++)
+for (i = 1; i <= 2; i++)
 {
 x *= atoi(argv[i]);
 }
