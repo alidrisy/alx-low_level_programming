395f5a78686bf2cfd1fa4dbd628ce66559a51777
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-10 13:26:48 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - multiplies two numbers.
+* @argc: numbers of arguments
+* @argv: pointer
+* Return: error or 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+x = 0;
+x *= atoi(argv[i]);
+}
