3cada3b72ec94864af344e1b0c923d70d6943a18
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-10 16:01:30 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the minimum number of coins
+* @argc: number og argument
+* @argv: a pointer
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+int cents, x, i;
+cents = atoi(argv[1])
+if (cents < 1)
+{
+printf("Error\n");
+return (1);
+}
+while (cents >= 1)
+{
+if (cents > 25)
+for (i = 0; cents > 24; i++)
+cents = cents - 25;
+else if (cents < 25 && cents > 9)
+for (; cents > 9; i++)
+cents = cents - 10;
+else if (cents < 10 && cents > 4)
+for (; cents > 4; i++)
+cents = cents - 5;
+else if (cents < 5 && cents > 1)
+for(; cents > 1; i++)
+cents = cents - 2;
+else if (cents == 1)
+for (; cents != 0; i++)
+cents = cents - 1;
+}
+printf("%d\n", i);
+return (0);
+}
