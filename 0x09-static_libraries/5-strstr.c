4c378ab4fa3603cf6801afbf989ae4ee38f4a6e1
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-10 03:42:52 +0300

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+* *_strstr - return the pointer to the occurrence
+* @haystack: char
+* @needle: char
+* Return: 0
+*/
 char *_strstr(char *haystack, char *needle)
 {
 return (0);
