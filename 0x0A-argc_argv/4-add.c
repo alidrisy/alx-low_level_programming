d1e4f24e67555a8a22cbe5bed1806ed685b3340d
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-10 14:27:54 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiplies two numbers.
+* @argc: numbers of arguments
+* @argv: pointer
+* Return: 1 or 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i, x;
+if (argc == 1 || argc == 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+x = 1;
+for (i = 1; i < 3; i++)
+{
+x *= atoi(argv[i]);
+}
+printf("%d\n", x);
+}
+return (0);
+}
