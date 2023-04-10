c3f4e795436af2d85a35c633d6815fe287411f07
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-10 13:00:25 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print the number of value the input
+* @argc: the number of value
+* @argv: pointer
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void)*argv;
+if (argc > 0)
+{
+printf("%d\n", argc - 1);
+}
+return (0);
+}
