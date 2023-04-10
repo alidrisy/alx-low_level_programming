d448143e653de8c3720483a23f1b8cdd453d5bee
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-10 22:18:40 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,18 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
 * main - print name of file
+* @argc: the numbers of input
+* @argv: a pointer
 * Return: 0
 */
-int main (int argc, char *argv[])
+nt main (int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s", argv[i]);
-}
+
+
+
+printf("%s\n", argv[argc * 0]);
+
 return (0);
 }
