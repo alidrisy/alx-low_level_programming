7daaed3b48c8bfbeb292b2bcfcce1ed08fc68fa1
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-12 03:16:32 +0300

    He who is not courageous enough to take risks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,18 +13,18 @@ char *str_concat(char *s1, char *s2)
 {
 int i, v, c;
 char *x;
-if (s1 == '\0')
+if (s1 == NULL)
 s1 = "";
-if (s2 == '\0')
+if (s2 == NULL)
 s2 = "";
 for (i = 0; s1[i]; i++)
 ;
 for (v = 0; s2[v]; v++)
 ;
 x = malloc(sizeof(char) * (i + v + 1));
-if (x == '\0')
+if (x == NULL)
 {
-return ('\0');
+return (NULL);
 }
 c = 0;
 while (c < (i + v))
