791cbd60d28549887548a7b02e2a3853ba7b984d
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-14 04:38:59 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,6 +3,36 @@
 #include <stdlib.h>
 
 /**
+
+* *_doit - initialize a byte of memory.
+
+* @x: int
+
+* @ptr: char
+
+*
+
+* Return: pointer
+
+*/
+
+char *_doit(char *ptr, unsigned int x)
+
+{
+
+char *d = ptr;
+
+do
+
+*d++=0;
+
+while (--sizr != 0);
+
+return (d);
+
+}
+
+/**
 * *_calloc - allocates memory for an array, using malloc.
 * @nmemb: the array
 * @size: the size of bytes
@@ -14,9 +44,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void *x;
 if (nmemb == 0 || size == 0)
 return (NULL);
-x = malloc(sizeof(int) * size);
+x = malloc(nmemb * size);
 if (x == 0)
 return (NULL);
-
+_doit(x, nmemb * size)
 return (x);
 }
