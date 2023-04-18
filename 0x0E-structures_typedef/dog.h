f7bb0f06f442132c374b033dfdeec65ca01df78d
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-18 04:09:56 +0300

    Bobby

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef _DOG_H
 #define _DOG_H
+/**
+* struct dog - set three variables
+* @name: frist member char
+* @age: second member int
+* @owner: third
+* Description: this is
+*/
 
-struct dog{
+struct dog
+{
 char *name;
 float age;
 char *owner;
