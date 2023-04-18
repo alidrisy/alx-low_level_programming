90ed8214bc86016122214141aa9e6313f723bdb8
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-18 13:58:55 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* new_dog - prints a struct dog
+* @name: pointet to struct dog
+* @age: int
+* @owner: char
+* Return: pointer
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *x;
+int v, b;
+
+for (v = 0; name[v]; v++)
+;
+for (b =0; owner[b]; b++)
+;
+
+x = malloc(sizeof(dog_t));
+
+if (!x)
+{
+return (NULL);
+}
+x->name = malloc(v + 1);
+if (!x->name)
+{
+free(x);
+return (NULL);
+}
+x->owner = malloc(b + 1);
+if (!x->owner)
+{
+free(x->name);
+free(x);
+return (NULL);
+}
+return (x);
+}
