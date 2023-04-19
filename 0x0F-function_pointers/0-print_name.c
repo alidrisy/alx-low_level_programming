e60294cf52a33e2ae5eae9baaf62bf40ade3de10
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-19 14:35:04 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name.
+* @name: pointer to a name
+*
+* @f: function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (!f || !name)
+return;
+
+f(name);
+
+}
