279274a811ddf8a7309752eec51fd0365dff8903
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-20 13:22:00 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - returns the sum of all its parameters.
+* @n: the parameters
+* Return: 0 or the sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+va_list x;
+unsigned int s, i;
+
+if (n == 0)
+return (0);
+
+va_start (x, n);
+
+s = 0;
+for (i = 0; i < n; i++)
+s += va_arg(x, int);
+
+va_end(x);
+
+return (s);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This repo is about Variadic Functions and Const Function Const Function.
