d96627d4c0a24b1d7d1099a249888da94c7a5f3d
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-20 04:24:22 +0300

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 /**
 * get_op_func - selects the correct function to perform the operation
 * @s: the operation
-* Return : get_op_func (char)
+* Return: NULL or the function
 */
 
 int (*get_op_func(char *s))(int, int)
