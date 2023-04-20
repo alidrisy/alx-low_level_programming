246291a54af912c15bf60375aadd3da943ea6682
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-20 23:57:38 +0300

    1-print_numbers.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef _VARIADIC_FUNCTION_H
 #define _VARIADIC_FUNCTION_H
 
-_putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
