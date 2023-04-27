395e6fbce2d6dad9bbc4ab6869479831243e8715
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-27 15:24:41 -0700

    asm

---
+++ 0x12-singly_linked_lists/main.asm
@@ -0,0 +1,17 @@
+global start
+
+section .text
+
+start: mov rax, 1
+mov rdi, 1
+mov rsi, msg
+mov rdx, 1
+syscall
+mov rax, 60
+mov rdi, rdi
+syscall
+
+section   .data
+msg: db Hello, Holberton, 10
+' >main.asm
+
