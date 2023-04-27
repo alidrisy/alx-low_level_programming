6fcb52f8fdba363cb24fc2ad7320e505be136200
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-27 12:54:05 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* free_list - frees a list_t list.
+* @head: the head for the link list
+*/
+
+void free_list(list_t *head)
+{
+if (!head)
+return;
+
+free(head->str);
+free(head->len);
+free(head);
+}
