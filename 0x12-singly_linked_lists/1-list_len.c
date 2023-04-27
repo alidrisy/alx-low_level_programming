b7d29eaa0e22055581b807a8f5a264b90f4843d8
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-27 10:11:52 -0700

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* list_len - prints all the elements of a list_t list.
+* @h: the pointer to the head
+* Return: the number of nodes
+*/
+
+size_t list_len(const list_t *h)
+{
+int i = 0;
+while (h)
+{
+if (h->str == NULL)
+{
+i++;
+}
+else
+{
+i++;
+}
+h = h->next;
+}
+return (i);
+}
