3ba91c900761ffdc7e3a520f84d34fc2d24db446
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-29 13:07:21 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,17 +2,10 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
-* free_listint2 - frees a listint_t list.
-* @head: address the first element
-*/
-
 void free_listint2(listint_t **head)
 {
 listint_t *new = NULL;
 listint_t *ne = NULL;
-if (!head)
-return;
 
 new = *head;
 while (new != NULL)
