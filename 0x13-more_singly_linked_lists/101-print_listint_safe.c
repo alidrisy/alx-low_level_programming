0acdbcd5325a3ace2fd75dcbdd5c433663ede125
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-05-02 12:29:08 -0700

    add

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_listint_safe - deletes the node at index
+* @head: the first address
+* Return: the number of node.
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+listint_t *new = NULL;
+listint_t *ne = NULL;
+int i, x = 0;
+
+if (head == NULL)
+exit(98);
+
+new = head;
+
+while (new)
+{
+printf("[%p] %d\n", (void *)new, new->n);
+x++;
+new = new->next;
+ne = head;
+i = 0;
+while (i < x)
+{
+if (ne == new)
+{
+printf("-> [%p] %d\n", (void *)new, new->n);
+return (x);
+}
+ne = ne->next;
+i++;
+}
+}
+return (x);
+}
