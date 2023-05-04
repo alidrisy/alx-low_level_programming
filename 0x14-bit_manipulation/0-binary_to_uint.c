ca8e135b699a2328b198eae5657c1989cdc1c741
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-05-04 08:50:46 -0700

    converts a binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* binary_to_uint - converts a binary number to an unsigned int.
+* @b: is pointing to a string of 0 and 1 chars
+* Return: the converted number, or 0
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int i = 1;
+unsigned int x = 0;
+int n;
+unsigned int s;
+
+if (b == NULL)
+return (0);
+
+s = strlen(b);
+
+for (n = s-1; n >= 0; n--)
+{
+if (b[n] != 0 && b[n] != 1)
+return (0);
+if (b[n] == 1)
+{
+x += i;
+}
+
+i *= 2;
+}
+return (x);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+In the end of this project we will learn:
+- How to manipulate bits and use bitwise operators
+
