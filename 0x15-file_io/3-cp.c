c4b77625d266f74845b21a0ae84926f31b461d5e
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-05-09 02:54:58 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -45,7 +45,7 @@ if (x == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98); }
-i = open(argv[2], O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0664);
+i = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 if (i == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
