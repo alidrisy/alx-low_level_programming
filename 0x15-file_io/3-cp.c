048853ed2c9838cbe43c3cffd258a5bfb90c3742
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-05-09 02:41:50 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,13 +10,13 @@ void f_close(int x, int i)
 {
 if (close(x) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", x);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", x);
 exit(100);
 }
 
 if (close(i) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", i);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
 exit(100);
 }
 
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 {
 int x, i;
 ssize_t v = 1024, n;
-char c[1024];
+char *c[1024];
 
 if (argc != 3)
 {
@@ -43,25 +43,25 @@ exit(97); }
 x = open(argv[1], O_RDONLY);
 if (x == -1)
 {
-dprintf(STDERR_FILENO, "Error: Cant read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98); }
 i = open(argv[2], O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0664);
 if (i == -1)
 {
-dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99); }
 while (v == 1024)
 {
 v = read(x, c, 1024);
 if (v == -1)
 {
-dprintf(STDERR_FILENO, "Error: Cant read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98); }
 
 n = write(i, c, v);
 if (n == -1)
 {
-dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99); }
 }
 
