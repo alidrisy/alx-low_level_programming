bf0c610bf2c00edb3180217dfd5b7fb42eac7020
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-05-08 10:08:30 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/*
+* read_textfile - reads a text file and prints it.
+* @filename: pointer to the file.
+* @letters: the number of letters it should read.
+ * Return: the number of letters it could read.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t i, n;
+char *x;
+
+if (filename == NULL)
+return (0);
+
+x = malloc(letters * sizeof(char));
+if (x == NULL)
+return (0);
+
+i = open(filename, O_RDONLY);
+if (i = -1)
+return (0);
+
+n = write(STDOUT_FILENO, x, read(i, x, letters));
+if (n = -1)
+return (0);
+
+close (i);
+free(x);
+
+return (n);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+In this project we will learn :
+- How to create, open, close, read and write files
+- What are file descriptors
