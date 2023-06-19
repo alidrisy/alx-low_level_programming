539c7faad6a737173cfa779f5f2798bf725cf55f
alidrisy <email@example.comclear>
2023-06-20 01:01:17 +0300

    add 100-operations.so

---
+++ 0x18-dynamic_libraries/cul.c
@@ -0,0 +1,56 @@
+/**
+* add - add tow integer
+* @a: int
+* @b: int
+* Return: a + b
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* sub - subtract tow integer
+* @a: int
+* @b: int
+* Return: a - b
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* mul - multiply tow integer
+* @a: int
+* @b: int
+* Return: a * b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* div - div tow integer
+* @a: int
+* @b: int
+* Return: a / b
+*/
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* mod - module tow integer
+* @a: int
+* @b: int
+* Return: a % b
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
+
