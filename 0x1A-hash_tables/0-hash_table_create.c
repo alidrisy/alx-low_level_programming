f683aa9e2818b121fbbfa4094b9b4fb64aed3cf3
alidrisy <email@example.comclear>
2023-07-06 21:50:45 +0300

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(table->size));
+	table->array = malloc(table->size,sizeof(hash_node_t));
 
 	if (size == 0)
 		return (NULL);
