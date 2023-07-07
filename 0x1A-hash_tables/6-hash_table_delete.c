e100456998f9d86e93b70aea01f25cfbed8e08f5
alidrisy <email@example.comclear>
2023-07-07 22:21:53 +0300

    6. >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - retrieves a value associated with a key.
+* @ht: the hash table we want to delet
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *new;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			new = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+			free(new->key);
+			free(new->item->value);
+			free(new->item);
+			free(new);
+		}
+		free(ht->array[i]);
+	}
+	free(ht);
+	ht = NULL;
+}
