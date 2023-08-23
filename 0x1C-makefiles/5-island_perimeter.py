25e43abc55f1c5ecec9eb649bd268f85b8a660c5
alidrisy <a.alidrisy6@gmail.com>
2023-08-23 22:02:11 +0300

    fix errors

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,14 +12,19 @@ def island_perimeter(grid):
         return 0
     for li in grid:
         n = li.count(1)
-        if n:
+        if n == 1:
             if i == li.index(1) or i == 0:
                 x += 1
                 i = li.index(1)
             else:
                 i = 0
+                x += 2
+        elif n > 1:
+            i = li.index(1)
+            if n > y:
+                y = n
+            if li[i] == 1 and li[i+1] == 1:
                 x += 1
-        if n > y:
-            y = n
-
+            elif li[i] == 1 and li[i+1] == 0:
+                x += 2
     return (x * 2) + (y * 2)
