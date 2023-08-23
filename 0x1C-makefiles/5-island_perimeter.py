88711ef9a9ed6a152545e1d87fd2696464069b04
alidrisy <a.alidrisy6@gmail.com>
2023-08-23 21:06:00 +0300

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,15 +3,18 @@
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid"""
+    """Returns the perimeter of the island described in grid"""
     x = 0
+    n = 0
+    i = 0
+    y = 0
     for li in grid:
-        y = 0
-        for i in li:
-            y += i
-        if y > 1:
-            x += y + 1
-        else:
-            x += y
+        n = li.count(1)
+        if n:
+            if i == li.index(1) or i == 0:
+                x += 1
+                i = li.index(1)
+        if n > y:
+            y = n
 
-    return x * 2 
+    return (x * 2) + (y * 2)
