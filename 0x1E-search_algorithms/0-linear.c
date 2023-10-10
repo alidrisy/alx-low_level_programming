e7876883058f99b890e7c4d7125b50e927d0f385
alidrisy <a.alidrisy6@egamil.com>
2023-10-10 20:07:07 +0300

    1. Binary search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of 
+ * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
  * @array: is a pointer to the first element of the array to search in
- * size: is the number of elements in array
- * value: is the value to search for
+ * @size: is the number of elements in array
+ * @value: is the value to search for
  * Return: first index where value is located, or -1
  */
 
@@ -19,7 +19,6 @@ int linear_search(int *array, size_t size, int value)
 		if (array[i] == value)
 		{
 			return (i);
-			break;
 		}
 		i++;
 	}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: first index where value is located, or -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int i = 0;
+	int c;
+	int s = size - 1;
+
+	while (i <= s)
+	{
+		printf("Searching in array: ");
+		for (int x = i; x <= s; x++)
+		{
+			if (x != s)
+			printf("%d, ", array[x]);
+			else
+			printf("%d\n", array[x]);
+		}
+
+		if (((i + s) % 2) == 0)
+		c = ((i + s) / 2);
+		else
+		c = (((i + s) - 1) / 2);
+
+		if (array[c] < value)
+		i = c + 1;
+		else if (array[c] > value)
+		s = c - 1;
+		else
+		return (c);
+	}
+	return (-1);
+}
