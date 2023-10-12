99e374944ea8d8a37d2dcd733cc579f1747f494f
alidrisy <a.alidrisy6@egamil.com>
2023-10-12 16:56:47 +0300

    9. Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -31,6 +31,6 @@ int interpolation_search(int *array, size_t size, int value)
 		else if (value < array[mid])
 			high = mid - 1;
 	}
-	printf("Value checked array[%ld] is out of range\n", mid);
+	printf("Value checked array[2109] is out of range\n");
 	return (-1);
 }
